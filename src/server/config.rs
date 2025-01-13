8bf328d0c742a03e09b9e1344adfb5cf980ace3b
Christopher David <chris@openagents.com>
2025-01-13 09:31:21 -0600

    Update config.rs

--- src/server/config.rs
+++ src/server/config.rs
@@ -1,4 +1,3 @@
-use actix_files as fs;
 use actix_web::web;
 
 use super::{admin::middleware::AdminAuth, routes};
@@ -13,13 +12,4 @@ pub fn configure_app(cfg: &mut web::ServiceConfig) {
 
     // Configure non-admin routes
     routes::configure_routes(cfg);
-
-    // Serve static files from the static directory
-    cfg.service(
-        fs::Files::new("/", "./static")
-            .index_file("index.html")
-            .use_hidden_files()
-            .prefer_utf8(true)
-            .show_files_listing(),
-    );
 }
