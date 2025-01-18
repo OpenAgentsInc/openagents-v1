e67dcd2d21fc828b01c036c42b877968cdbd3e79
Christopher David <chris@openagents.com>
2025-01-18 16:53:31 -0600

    Update auth.rs

--- src/server/middleware/auth.rs
+++ src/server/middleware/auth.rs
@@ -54,9 +54,9 @@ where
 {
     type Rejection = AuthError;
 
-    async fn from_request_parts<B>(
+    async fn from_request_parts(
         parts: &mut Parts,
-        _: &S,
+        state: &S,
     ) -> Result<Self, Self::Rejection> {
         // Get cookies from the request
         let cookies = CookieJar::from_headers(&parts.headers);
