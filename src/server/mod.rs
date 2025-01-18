bd34fd6dc902ef7c06c9467cf59a6bdd6614ff09
Christopher David <chris@openagents.com>
2025-01-17 21:51:34 -0600

    Update mod.rs

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,6 +1,8 @@
 pub mod admin;
 pub mod config;
+pub mod handlers;
 pub mod middleware;
 pub mod services;
 
+pub use handlers::{login, callback, logout};
 pub use middleware::{AuthenticatedUser, AuthError};
