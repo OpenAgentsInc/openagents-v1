b9b8baf0aa021a23ee65f484362bd8e5e19c17dd
Christopher David <chris@openagents.com>
2025-01-18 16:52:15 -0600

    Update auth.rs

--- src/server/middleware/auth.rs
+++ src/server/middleware/auth.rs
@@ -54,7 +54,7 @@ where
 {
     type Rejection = AuthError;
 
-    async fn from_request_parts(
+    async fn from_request_parts<B>(
         parts: &mut Parts,
         _: &S,
     ) -> Result<Self, Self::Rejection> {
