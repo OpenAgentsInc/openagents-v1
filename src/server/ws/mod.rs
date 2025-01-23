4d0f9ed9cb688f114b5d7c74c12be378c231e7b5
Christopher David <chris@openagents.com>
2025-01-22 21:32:40 -0600

    Update mod.rs

--- src/server/ws/mod.rs
+++ src/server/ws/mod.rs
@@ -1,7 +1,9 @@
 pub mod handlers;
 pub mod transport;
 pub mod types;
+pub mod handler;
 
 pub use handlers::chat::ChatHandler;
 pub use handlers::solver::SolverHandler;
 pub use types::{Message, WebSocketState};
+pub use handler::ws_handler;
